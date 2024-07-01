2c537bcda38a8cff4c3e564a9a2e29a3d83100e5
sablu1234 <150502592+sablu1234@users.noreply.github.com>
2024-07-01 20:54:49 +0600

    while/for /for reach /do while loop

--- index.php
+++ index.php
@@ -1,18 +1,37 @@
 <?php
-// $name= 'hasan';
-// echo $name;
 
-// bloginfo('url');
+// $x= 2;
+// while($x<=5){
+//     echo $x."<br>";
+//     $x+=2;
+// }
+
+// $x=1;
+// do{
+// echo $x."<br>";
+// $x++;
+// }
+// while($x<1);
 
 
-$all_post= wp_count_posts()->publish;
-echo $all_post.'<br>';
+// for($x = 1; $x < 5; $x++){
+// echo $x."<br>";
+// }
 
-$hasan_result=$all_post;
- 
-while($hasan_result<10){
-    echo $hasan_result;
-    $hasan_result++;
+
+
+// $name= array('Saki','Ahnaf','Monira');
+// foreach($name as $value){
+// echo $value.' ';
+// }
+
+
+
+
+for($x = 1; $x<6; $x++){
+    if($x==3){
+        continue;
+    }
+echo $x."<br>";
 }
-
 ?>
---
+++ whileloop.php
@@ -0,0 +1,20 @@
+<?php
+// $name= 'hasan';
+// echo $name;
+
+// bloginfo('url');
+
+
+// $all_post= wp_count_posts()->publish;
+// echo $all_post.'<br>';
+
+// $hasan_result=$all_post;
+ 
+// while($hasan_result<10){
+//     echo $hasan_result;
+//     $hasan_result++;
+// }
+
+
+
+?>
